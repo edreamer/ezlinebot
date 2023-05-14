63017f1a4a5a0882e4684e36d368b2431db35f77
eDreamer <edreamer@gmail.com>
2023-05-14 21:09:14 +0800

    Update index.py

--- index.py
+++ index.py
@@ -16,13 +16,12 @@ def callback():
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
-        print("Invalid signature. Please check your channel access token/channel secret.")
         abort(400)
     return 'OK'
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=event.message.text))
+    line_bot_api.reply_message(event.reply_token,TextSendMessage(text=event.message.text))
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run()

03c6861c0ffec550d6c9ee6cf206d2789111f671
eDreamer <edreamer@gmail.com>
2023-05-14 23:00:44 +0800

    Update index.py

--- index.py
+++ index.py
@@ -1,6 +1,5 @@
 from flask import Flask, request, abort
 import os
-
 from linebot import  LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage

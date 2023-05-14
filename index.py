724e75ecb85d9c9e915c0c6566eea6dd93a821e4
eDreamer <edreamer@gmail.com>
2023-05-14 20:43:37 +0800

    Update index.py

--- index.py
+++ index.py
@@ -1,39 +1,27 @@
-from flask import Flask, request, abort
-
-from linebot import  LineBotApi, WebhookHandler
-from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage,
-
+from flask import Flask
 app = Flask(__name__)
 
-line_bot_api = LineBotApi('srFTsHK62OAvxrtPQxwkyYdXrS0ICYgrSgKarEpd5ayHlSMPY2TEP1KNbDMUm5bx4cDb7610wTfayRUG+ZXOkPrPt6zwIP5VrmGLJnqSvioWwxVL7m4g9Vr9OqNcr1ucsS2fxqWFCsajmWxfN32mTgdB04t89/1O/w1cDnyilFU=')
-handler = WebhookHandler('3422ef0232368541bcf7867e2f7f8443')
+from flask import request, abort
+from linebot import  LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+
+line_bot_api = LineBotApi('Wg7R/Gpo7g7TLAZXhNgHu7mw+ADzNTBk4DYc3/Qnr53ahMBl4WR9IzaW+2w8Tk8TcRbHF0y+9SahfLTUxjj55QugXurAip/5ExuIERSc5fb2qz03PCVLKPuTNTmu7W/ENZ310wepUZZihjrmi9c2LQdB04t89/1O/w1cDnyilFU=')
+handler1 = WebhookHandler('0f91a5c0a4c8b3f02693b5d35fa17877')
 
 @app.route("/callback", methods=['POST'])
 def callback():
-    # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
-
-    # get request body as text
     body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
-
-    # handle webhook body
     try:
-        handler.handle(body, signature)
+        handler1.handle(body, signature)
     except InvalidSignatureError:
-        print("Invalid signature. Please check your channel access token/channel secret.")
         abort(400)
-
     return 'OK'
 
-
-@handler.add(MessageEvent, message=TextMessage)
+@handler1.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
+    line_bot_api.reply_message(event.reply_token,TextSendMessage(text=event.message.text))
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run()

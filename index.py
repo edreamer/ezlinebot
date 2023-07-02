93bb38ed58b88836b21815715a46eeee8782f1cd
eDreamer <edreamer@gmail.com>
2023-07-03 06:52:26 +0800

    Update index.py

--- index.py
+++ index.py
@@ -32,7 +32,7 @@ def askchatgpt(q):
 app = Flask(__name__)
 
 line_bot_api = LineBotApi(os.getenv('LINE_ACCESS_TOKEN'))
-handler = WebhookHandler(os.getenv('LINE_CHANNEL_SECRET'))
+handler1 = WebhookHandler(os.getenv('LINE_CHANNEL_SECRET'))
 
 
 @app.route("/callback", methods=['POST'])
@@ -46,7 +46,7 @@ def callback():
 
     # handle webhook body
     try:
-        handler.handle(body, signature)
+        handler1.handle(body, signature)
     except InvalidSignatureError:
         print("Invalid signature. Please check your channel access token/channel secret.")
         abort(400)
@@ -54,7 +54,7 @@ def callback():
     return 'OK'
 
 
-@handler.add(MessageEvent, message=TextMessage)
+@handler1.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
